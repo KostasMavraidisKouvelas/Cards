7391a5416fd688b81a539224003bb56de410dea0
KostasMavraidis <84708169+KostasMavraidis@users.noreply.github.com>
2024-02-26 00:15:03 +0200

    Add project files.

---
+++ CardsWeb.Models/Card.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Runtime.InteropServices;
+using CardsWeb.Models.Base;
+using CardsWeb.Models.Enums;
+
+namespace CardsWeb.Models
+{
+    public class Card : BaseModel
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [RegularExpression("^#[a-zA-Z0-9]{6}$")]
+        public string Color { get; set; }
+
+        public Status Status { get; set; } = Status.ToDo;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
---
+++ CardsWebApi/Controllers/CardController.cs
@@ -0,0 +1,16 @@
+﻿using CardsWeb.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CardsWeb.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CardController : Controller
+    {
+        public async Task<IActionResult> GetById()
+        {
+            return Ok(new Card());
+        }
+    }
+}
