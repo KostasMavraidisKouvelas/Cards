99c2f4e38c69a20c26dfade8b4c148d18af179b8
KostasMavraidis <84708169+KostasMavraidis@users.noreply.github.com>
2024-02-26 00:34:44 +0200

    Adding NugetPackages,Dependencies and some other changes

---
+++ CardsWeb.DataAccess/CardsDbContext.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using CardsWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CardsWeb.DataAccess
+{
+    public class CardsDbContext : DbContext
+    {
+        public CardsDbContext(DbContextOptions<CardsDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        protected override void //#D //#A
+            OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+
+
+        }
+        public virtual DbSet<User> Users { get; set; }
+        public virtual DbSet<Card> Cards { get; set; }
+    }
+}
